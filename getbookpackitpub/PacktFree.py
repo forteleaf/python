3bfc9a50dc9ac5b0ac626c6fbd434310e79e4492
forteleaf <forteleaf@live.com>
2016-06-17 14:16:47 +0900

    simple

--- getbookpackitpub/PacktFree.py
+++ getbookpackitpub/PacktFree.py
@@ -5,32 +5,36 @@
 
 from bs4 import BeautifulSoup
 import sys
-import urllib, http.cookiejar
+import urllib
+import http.cookiejar
 import requests
 import re
+
 
 # geturl and getbook
 def getbook(email, password):
     print("free-learing 주소를 얻어 오는 중...")
     base_url = "https://www.packtpub.com/"
     authentication_url = "https://www.packtpub.com/packt/offers/free-learning"
-    header = {'User-Agent':'Mozilla/5.0'}
-    r=requests.post(authentication_url,headers=header)
-    r.encoding='utf-8'
+    header = {'User-Agent': 'Mozilla/5.0'}
+    r = requests.post(authentication_url, headers=header)
+    r.encoding = 'utf-8'
 
     # 무료책 받는 url 찾기
-    bs4_packt = BeautifulSoup(r.text,"html.parser")
-    getBook_url = base_url+bs4_packt.find('a',attrs={'class':'twelve-days-claim'})['href']
+    bs4_packt = BeautifulSoup(r.text, "html.parser")
+    getBook_url = base_url + \
+        bs4_packt.find('a', attrs={'class': 'twelve-days-claim'})['href']
 
     # Cookie 를 저장
     cj = http.cookiejar.LWPCookieJar()
-    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+    opener = urllib.request.build_opener(
+        urllib.request.HTTPCookieProcessor(cj))
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     urllib.request.install_opener(opener)
 
     # 정보입력
     value = {
-        'op':'Login',
+        'op': 'Login',
         'email': email,
         'password': password
     }
@@ -41,22 +45,20 @@ def getbook(email, password):
     req = urllib.request.Request(authentication_url, data)
     resp = urllib.request.urlopen(req)
     cookie = resp.headers.get('Set-Cookie')
-
     contents = resp.read()
-    #확인용
+    # 확인용
     # print(getBook_url)
     # print(cookie)
-# print(contents)
+    # print(contents)
 
 print('packtpub 무료책을 자동으로 받아오기')
-#사용자정보
+# 사용자정보
 user_email = input("email 을 입력해 주세요.\n")
-#email 형식 체크
+# email 형식 체크
 EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 if not EMAIL_REGEX.match(user_email) or user_email == "":
     print('email이 형식이 틀렸습니다..')
     sys.exit()
 user_pass = input('비밀번호를 입력해 주세요.\n')
-
-getbook = getbook(user_email,user_pass)
+getbook = getbook(user_email, user_pass)
 print("완료 되었습니다.")
